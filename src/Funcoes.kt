80f914e1408e1600ecb320624a7e7648d4b81b9a
Lucas Silva <lucsilva1999@gmail.com>
2021-05-24 16:44:19 -0300

    Kotlin - Aula 04 de funções. Corpo x Expressão

--- src/Funcoes.kt
+++ src/Funcoes.kt
@@ -3,9 +3,16 @@ import kotlin.reflect.typeOf
 fun main() {
     val firstName = "Lucas"
     val lastName = "Silva"
-    println("Olá $firstName $lastName, o resultado da função é: ${max(10, 5)} ")
+    println("Olá $firstName $lastName, o resultado da função é: ${maxCrp(10, 5)} ")
+    println("Olá $firstName $lastName, o resultado da função é: ${maxExp(10, 5)} ")
 }
 
-fun max(a: Int, b: Int): Int {
+// Bloco de Corpos
+// Função avançadas que pode ter várias expressões, validações ou valores
+fun maxCrp(a: Int, b: Int): Int {
     return if (a > b) a else b
 }
+
+// Corpo de Expressão
+// Função simples com até uma expressão, validação ou valor
+fun maxExp(a: Int, b: Int): Int = if (a > b) a else b
