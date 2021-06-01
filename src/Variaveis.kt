55750f8af4bd47f7d0bb500e2a8a24baa68c3228
Lucas Silva <lucsilva1999@gmail.com>
2021-05-31 22:36:29 -0300

    Kotlin - Aula 05 de variáveis Inferência x Declaração com veriicação do tipo de variável.

---
+++ src/Variaveis.kt
@@ -0,0 +1,23 @@
+import kotlin.reflect.typeOf
+
+fun main() {
+   var idade = 1
+    while (idade < 5) {
+        idade++
+        println("O valor atual da variável idade é ${idade}")
+    }
+
+    //VAL = imutávels
+    //VAR = mutávels
+
+    //Inferência do Tipo
+    val fruta = "Abacaxi"
+
+    //Declaração do tipo
+    var fruta2 = "Banana"
+    println("A variável ${idade} é do tipo ${idade::class.simpleName}")
+    println("A variável imutável ${fruta} é do tipo: ${fruta::class.simpleName}")
+    println("A variável mutável ${fruta2} é do tipo: ${fruta::class.simpleName}")
+    println("O valor imutável é ${fruta} e o valor mutável é ${fruta2}")
+
+}
